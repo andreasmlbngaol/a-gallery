fbdb82d0f6adf4d58d013aa2a939fdb84a5d12e5
andreasmlbngaol <lgandre45@gmail.com>
2026-07-06 07:59:59 +0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "id.andreasmbngaol.agallery"
         minSdk = 29
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
